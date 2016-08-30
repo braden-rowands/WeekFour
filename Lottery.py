03abb61cbdd44a2ad27b7a4ad36fb73c164031a8
Braden <browl1742@gmail.com>
2016-08-30 18:49:13 +1000

    fixed up errors

--- Lottery.py
+++ Lottery.py
@@ -1,14 +1,9 @@
 import random
-amountOfPicks = input ("Please enter the amount of picks you would like")
-#while amountOfPicks != amountOfPicks.isnumeric():
-#    amountOfPicks = input("Please enter the amount of picks you would like")
-quickPicks = []
-secondCounter = 0
-counter = 0
-numbers = 6
-#while secondCounter < int(amountOfPicks):
-while counter < numbers:
-    quickPicks.append(random.randrange(1,45))
-    counter += 1
-    #secondCounter =+1
-print(quickPicks[::])
+thelist = []
+amountOfPicks = input("Please enter the amount of picks you would like")
+for i in range (int(amountOfPicks)):
+    for number in range(6):
+        number = random.randint(1,45)
+        thelist.append(number)
+    print(thelist)
+    thelist = []
